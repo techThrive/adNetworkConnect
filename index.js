dc7250124a16c303c8e4012da05ebbb7f7cc3a5f
Tamojit Pal <tamojitp@gmail.com>
2026-06-13 15:19:36 +0000

    Add Interakt test endpoint

--- index.js
+++ index.js
@@ -20,44 +20,21 @@ app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
 
-app.get("/send-test", async (req, res) => {
-
-  try {
-
-    const response = await axios.post(
-      "https://api.interakt.ai/v1/public/message/",
-      {
-        countryCode: "+91",
-        phoneNumber: "8147110152",
-        callbackData: "adnetwork-test",
-        type: "Template",
-        template: {
-          name: "network_alert",
-          languageCode: "en",
-          bodyValues: [
-            "Router-01",
-            "DOWN",
-            "Interface Gi0/1 is down"
-          ]
-        }
-      },
-      {
-        headers: {
-          Authorization: `Basic ${process.env.INTERAKT_API_KEY}`,
-          "Content-Type": "application/json"
-        }
-      }
-    );
-
-    res.json(response.data);
-
-  } catch (err) {
-
-    console.error(err.response?.data || err.message);
-
-    res.status(500).json(
-      err.response?.data || { error: err.message }
-    );
+const response = await axios.post(
+  "https://api.interakt.ai/v1/public/message/",
+  {
+    countryCode: "+91",
+    phoneNumber: "8147110152",
+    type: "Template",
+    template: {
+      name: "icc_championship_2",
+      languageCode: "en"
+    }
+  },
+  {
+    headers: {
+      Authorization: `Basic ${process.env.INTERAKT_API_KEY}`,
+      "Content-Type": "application/json"
+    }
   }
-
-});
+);
